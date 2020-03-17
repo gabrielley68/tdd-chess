0b5e6f21b083987b3ecdb7b8e2c245c62942acb1
Ley Gabriel <gabriel.ley@outlook.fr>
2020-03-17 15:41:23 +0100

    Test date on creation

--- src/test/java/org/mydigitalschool/tdd/chess/game/EngineTest.java
+++ src/test/java/org/mydigitalschool/tdd/chess/game/EngineTest.java
@@ -1,5 +1,19 @@
 package org.mydigitalschool.tdd.chess.game;
 
+import static org.junit.Assert.*;
+
+import java.time.LocalDateTime;
+
+import org.junit.Test;
+
 public class EngineTest {
+	
+	@Test
+	public void testDateOnCreation() {
+		LocalDateTime time = LocalDateTime.now();
+		Engine engine = new Engine();
+		
+		assertTrue(time.isBefore(engine.getStartTime()) || time.isEqual(engine.getStartTime()));
+	}
 
 }
