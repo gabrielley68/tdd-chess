4188e54260fe83712901c414052479a3bdc24ad8
Ley Gabriel <gabriel.ley@outlook.fr>
2020-03-17 12:08:34 +0100

    deleted old files

--- src/main/java/com/tactfactory/monprojet/databases/ArticleRepository.java
+++
@@ -1,9 +0,0 @@
-package com.tactfactory.monprojet.databases;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-import com.tactfactory.monprojet.entities.Article;
-
-public interface ArticleRepository extends JpaRepository<Article, Long> {
-
-}
--- src/main/java/com/tactfactory/monprojet/databases/UserRepository.java
+++
@@ -1,9 +0,0 @@
-package com.tactfactory.monprojet.databases;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-import com.tactfactory.monprojet.entities.User;
-
-public interface UserRepository extends JpaRepository<User, Long> {
-
-}
