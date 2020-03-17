6ec56f4db892a5090259bfee768daba0834a4414
Alexis <alexis.m@france-diffusion.com>
2020-03-17 16:27:15 +0100

    Test one piece in one box

--- src/test/java/org/mydigitalschool/tdd/chess/game/BoardTest.java
+++ src/test/java/org/mydigitalschool/tdd/chess/game/BoardTest.java
@@ -1,5 +1,31 @@
 package org.mydigitalschool.tdd.chess.game;
 
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
 public class BoardTest {
 
+	@Test
+	public void testShowChestBoardWhite() {
+		
+	}
+	
+	@Test
+	public void testShowChestBoardBlack() {
+	}
+	
+	@Test
+	public void testOnePiece() {
+		Board board = new Board();
+		board.setPiece(1,2,4);
+		board.setPiece(1,2,1);
+		assertEquals("Impossible de faire ceci!", board.msgErreur);
+	}
+	
+	@Test
+	public void testPieceOnChestBoard() {
+		
+	}
+	
 }
