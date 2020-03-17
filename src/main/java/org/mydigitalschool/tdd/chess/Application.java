99383494b0540aaa5be5f95ec7bfc4581d3eb74e
Ley Gabriel <gabriel.ley@outlook.fr>
2020-03-17 15:06:31 +0100

    Created base class

---
+++ src/main/java/org/mydigitalschool/tdd/chess/Application.java
@@ -0,0 +1,10 @@
+package org.mydigitalschool.tdd.chess;
+
+public class Application {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
---
+++ src/main/java/org/mydigitalschool/tdd/chess/game/pieces/Piece.java
@@ -0,0 +1,5 @@
+package org.mydigitalschool.tdd.chess.game.pieces;
+
+public abstract class Piece {
+
+}
