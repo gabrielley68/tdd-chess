68acf21a539f62a789ae8082cd43ce760db4c9ed
Ley Gabriel <gabriel.ley@outlook.fr>
2020-03-17 16:10:28 +0100

    Test player different colors

--- src/test/java/org/mydigitalschool/tdd/chess/game/EngineTest.java
+++ src/test/java/org/mydigitalschool/tdd/chess/game/EngineTest.java
@@ -5,6 +5,7 @@ import static org.junit.Assert.*;
 import java.time.LocalDateTime;
 
 import org.junit.Test;
+import org.junit.
 
 public class EngineTest {
 	
@@ -17,6 +18,14 @@ public class EngineTest {
 		Engine engine = new Engine();
 		// startTime may be right before time due to execution time
 		assertTrue(time.isBefore(engine.getStartTime()) || time.isEqual(engine.getStartTime()));
+	}
+	
+	/**
+	 * Assert that two players doesn't have the same color on creation
+	 */
+	public void playerColorDifferent() {
+		Engine engine = new Engine();
+		assertNotEquals(engine.getPlayer1().getColor(), engine.getPlayer2().getColor());
 	}
 
 }
